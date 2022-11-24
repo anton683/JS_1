487abbac56f21b5a33e2b168199f87265315fded
Anton <antonio37rus@gmail.com>
2022-11-24 16:57:14 +0300

    test

---
+++ test_1.js
@@ -0,0 +1,65 @@
+// console.log(typeof typeof 1);
+// String
+
+// console.log(false == '0');
+// true
+
+// оператор === сравнивает без приведения типов
+
+/*
+let a;
+switch (a) {
+  case 0:
+    console.log('0');
+  case null:
+    console.log('null');
+  case undefined:
+    console.log('undefined');
+  default:
+    console.log('default');
+}
+undefined
+default
+*/
+
+/*
+let a = 10;
+let answer = 10 != a ? "Yes" : "No";
+console.log(answer); 
+No
+*/
+
+// alert("12" / 0);   infinity
+
+// alert(0 / 0);  NaN
+
+/*
+ error = "Слишком короткий пароль!"
+console.log(error);
+var error = "Слишком длинный пароль!"
+*/
+
+/*
+const error;
+console.log(message);
+message = "Слишком короткий пароль!";
+ошибка 
+*/
+
+/* 
+console.log(error);
+let error = "Слишком короткий пароль!";
+ошибка
+*/
+
+/*
+let num = 10;
+if (num % 3 === 0) {
+  num = 5;
+}
+console.log(num);
+10
+*/
+
+// alert(null + undefined); NaN 
+
