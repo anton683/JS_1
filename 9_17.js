d7c26ed9ff55ef5c8ce8a21c0d9c98070467ad5b
Anton <antonio37rus@gmail.com>
2022-12-10 18:43:44 +0300

    9.17

--- 9_17.js
+++ 9_17.js
@@ -1,3 +1,15 @@
+/*
+В программе объявлена переменная obj, содержащая объект с подобным набором значений:
+{
+        "Яблоко": "фрукт",
+        "Арбуз": "ягода",
+        "Помидор": "овощ",
+        "Огурец": "овощ",
+        "Вишня": "ягода"
+}
+Напишите скрипт, который формирует новый объект со значениями и их количеством. Выведите новый объект в консоль.
+*/
+
 let obj = {
     "Яблоко": "фрукт",
     "Арбуз": "ягода",
@@ -6,17 +18,13 @@ let obj = {
     "Вишня": "ягода"
 };
 
-hash = {};
-
-for (let k in obj) {
-    // console.log(obj[k]);
-    if (hash[obj[k]]) {
-        hash[obj[k]]++;
-    } else {
-        hash[obj[k]] = 1;
+let newObj = {};
+for (let value of Object.values(obj)) {
+    if (newObj[value] === undefined) {
+        newObj[value] = 1
+        continue
     }
-    // v2
-    // let result = hash[obj[k]];
-    // hash[obj[k]] = result ? ++result : 1
+    newObj[value] += 1
 }
-console.log(hash);
+
+console.log(newObj);
